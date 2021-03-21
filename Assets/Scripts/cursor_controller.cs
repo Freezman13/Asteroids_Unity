df80023ae5ad9c1e14653a37462fc75f81ca3597
Chiril Russu <58755601+Freezman13@users.noreply.github.com>
2021-03-20 19:41:41 -0500

    Added cursor

---
+++ Assets/Scripts/cursor_controller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cursor_controller : MonoBehaviour
+{
+    // initializing the cursor
+    public Texture2D cursor;
+
+    private void change_cursor(Texture2D cursor_type)
+    {
+        Vector2 hotspot = new Vector2(cursor_type.width / 2, cursor_type.height / 2);
+        Cursor.SetCursor(cursor_type, hotspot, CursorMode.Auto);
+    }
+
+  //  private void Awake()
+    //{
+        
+  //  }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        change_cursor(cursor);
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
